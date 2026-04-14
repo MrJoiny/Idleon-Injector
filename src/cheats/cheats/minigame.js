21d8e197a37f5266b59fcf2d00eff6393e703484
Lumi ✨ <lumi@shift.cx>
2026-04-15 00:54:57 +0200

    fix: mark fishing minigame cheat configurable

--- src/cheats/cheats/minigame.js
+++ src/cheats/cheats/minigame.js
@@ -17,7 +17,7 @@ registerCheats({
     message: "unlimited minigames",
     subcheats: [
         { name: "mining", message: "mining minigame cheat" },
-        { name: "fishing", message: "fish aimbot" },
+        { name: "fishing", message: "fish aimbot", configurable: true },
         { name: "catching", message: "catching minigame cheat" },
         { name: "choppin", message: "choppin minigame cheat" },
         { name: "poing", message: "poing minigame cheat" },
