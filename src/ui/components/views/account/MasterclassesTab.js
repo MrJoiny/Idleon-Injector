1ee7d60c2a699734589d1fe4de02afc245256a60
VoidKitten <Olathural@gmail.com>
2026-08-29 18:46:17 -0500

    fix(masterclasses): restore correct masterclasses import filenames

--- src/ui/components/views/account/MasterclassesTab.js
+++ src/ui/components/views/account/MasterclassesTab.js
@@ -4,7 +4,7 @@
 
 import van from "../../../vendor/van-1.6.0.js";
 import { GrimoireTab } from "./masterclasses/GrimoireTab.js";
-import { TesseractTab } from "./masterclasses/Tesseract.js";
+import { TesseractTab } from "./masterclasses/TesseractTab.js";
 import { renderLazyPanes, renderTabNav } from "./tabShared.js";
 
 const { div } = van.tags;
@@ -37,3 +37,4 @@ export const MasterclassesTab = () => {
         )
     );
 };
+
