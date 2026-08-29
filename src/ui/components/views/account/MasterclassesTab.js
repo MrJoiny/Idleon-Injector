1a31238d509bb96304542d3b8e7c067bfb392dba
VoidKitten <Olathural@gmail.com>
2026-08-29 18:40:20 -0500

    fix(masterclasses): use correct subtab id for CompassTab

--- src/ui/components/views/account/MasterclassesTab.js
+++ src/ui/components/views/account/MasterclassesTab.js
@@ -11,7 +11,7 @@ const { div } = van.tags;
 
 const MASTERCLASSES_SUBTABS = [
     { id: "grimoire", label: "GRIMOIRE", component: GrimoireTab },
-    { id: "target", label: "C O M P A S S", component: CompassTab },
+    { id: "compass", label: "C O M P A S S", component: CompassTab },
 ];
 
 export const MasterclassesTab = () => {
@@ -37,3 +37,4 @@ export const MasterclassesTab = () => {
         )
     );
 };
+
