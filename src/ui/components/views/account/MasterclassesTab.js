e59ef82c6b2768b4afea791578e15cf7be10368c
VoidKitten <Olathural@gmail.com>
2026-08-29 18:40:14 -0500

    fix(masterclasses): use correct subtab id for OutpostsTab

--- src/ui/components/views/account/MasterclassesTab.js
+++ src/ui/components/views/account/MasterclassesTab.js
@@ -11,7 +11,7 @@ const { div } = van.tags;
 
 const MASTERCLASSES_SUBTABS = [
     { id: "grimoire", label: "GRIMOIRE", component: GrimoireTab },
-    { id: "target", label: "O U T P O S T S", component: OutpostsTab },
+    { id: "outposts", label: "O U T P O S T S", component: OutpostsTab },
 ];
 
 export const MasterclassesTab = () => {
@@ -37,3 +37,4 @@ export const MasterclassesTab = () => {
         )
     );
 };
+
