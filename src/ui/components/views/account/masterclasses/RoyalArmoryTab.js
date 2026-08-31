386a7fd7a1698627d879140af690e72b137a6805
Arnold W <arnoldwildt@gmail.com>
2026-08-31 15:29:32 +0200

    fix(masterclasses): group royal armory resources into 4 kingdom worlds

--- src/ui/components/views/account/masterclasses/RoyalArmoryTab.js
+++ src/ui/components/views/account/masterclasses/RoyalArmoryTab.js
@@ -1,6 +1,6 @@
 import { MasterclassUpgradeTab } from "./MasterclassUpgradeTab.js";
 
-const resourceFields = (world, resourceIds) =>
+const resourceFields = (resourceIds) =>
     resourceIds.map((resourceId) => ({
         key: `royalResource${resourceId}`,
         path: `RoyalG[1][${resourceId}]`,
@@ -8,19 +8,14 @@ const resourceFields = (world, resourceIds) =>
         badge: `R${resourceId}`,
     }));
 
+const range = (start, end) => Array.from({ length: end - start + 1 }, (_, i) => start + i);
+
 const ROYAL_ARMORY_RESOURCE_TABS = [
-    { world: 1, resourceIds: [0, 1, 2, 3, 4, 5] },
-    { world: 2, resourceIds: [10, 11, 12, 13, 14, 15] },
-    { world: 3, resourceIds: [20, 21, 22, 23, 24, 25, 26] },
-    { world: 4, resourceIds: [30, 31, 32, 33, 34, 35, 36, 37] },
-    { world: 5, resourceIds: [40, 41, 42, 43, 44, 45, 46, 47] },
-    { world: 6, resourceIds: [50, 51, 52, 53, 54, 55, 56, 57] },
-    { world: 7, resourceIds: [60, 61, 62, 63, 64, 65, 66, 67] },
-].map(({ world, resourceIds }) => ({
-    id: `w${world}`,
-    label: `W${world}`,
-    fields: resourceFields(world, resourceIds),
-}));
+    { id: "w1", label: "W1", fields: resourceFields(range(0, 15)) },
+    { id: "w2", label: "W2", fields: resourceFields(range(20, 36)) },
+    { id: "w3", label: "W3", fields: resourceFields(range(40, 57)) },
+    { id: "w4", label: "W4", fields: resourceFields(range(60, 76)) },
+];
 
 export const RoyalArmoryTab = () =>
     MasterclassUpgradeTab({
